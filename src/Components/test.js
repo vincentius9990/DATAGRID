f0780f0ce443419edfa12ce0b85f74c37d5c988f
vincent <vincentrkumar98@gmail.com>
2024-02-16 13:52:08 +0530

    changes

--- src/Components/test.js
+++
@@ -1,8 +0,0 @@
-<Textfield type="email" error={emailerr} onChange={emailhandler} label="Enter Email " InputProps={{
-    startAdornment: (
-        <InputAdornment position="start">
-            <EmailIcon />
-        </InputAdornment>
-    ),
-}}   margin="dense" fullWidth={true} />
- &&pass.length>=4&&pass.length<=10
